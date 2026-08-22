69b58973d13a8590f30bd1f37ac360def09ac08f
Harsh Raj <harshraj.work021@gmail.com>
2026-08-22 11:36:58 +0530

    Add Pattern087

---
+++ ch2_Pattern/Pattern9.java
@@ -0,0 +1,24 @@
+package ch2_Pattern;
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n= 10;
+
+        for(int r= 1; r<=n ; r++){
+            if(r==1 || r== 2 || r==n){
+                for(int c= 1;  c<=r;c++){
+                    System.out.print("*");
+                }
+            }
+            else {
+                System.out.print("*");
+                for(int c=1;c<=(r-2); c++){
+                    System.out.print(" ");
+                }
+              System.out.print("*");
+            }
+              System.out.println();
+        }
+        
+    }
+    
+}
